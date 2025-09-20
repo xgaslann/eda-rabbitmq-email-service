c97b1324dccc58c064ae95a1324d33fd71f00446
xgaslann <gorkem_aslan0@hotmail.com>
2025-09-20 22:48:16 +0300

    xgaslan initial

---
+++ src/main/java/com/xgaslan/edarabbitmqemailservice/EdaRabbitmqEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xgaslan.edarabbitmqemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaRabbitmqEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdaRabbitmqEmailServiceApplication.class, args);
+    }
+
+}
